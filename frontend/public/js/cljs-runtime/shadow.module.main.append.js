531d6c768022351687d86b2ffcee404ef6a2bce9
CountZeroNM <countzeronm@gmail.com>
2025-12-20 09:06:59 +0900

    chore: ignore frontend build artifacts

--- frontend/public/js/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { shio_walk.core.init(); } catch (e) { console.error("An error occurred when calling (shio-walk.core/init)"); console.error(e); }
